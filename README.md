446053d88cd3c0702e56752c3a4db430fa9b55fd
dcharlaftis <dcharlaftis@dotbydot.gr>
2016-01-08 11:34:17 +0200

    test

--- README.md
+++ README.md
@@ -1,3 +1,15 @@
 ﻿# adtMessages
 
+Node Application for manipulating adt messages in Humber hospital food services
+
+
+## usage
+
+[1] node app2.js for initiating adt server
+
+[2] node logParser.js for reading the whole log database again faster, after update
+
+
+
+
 
--- config/config-example.js
+++ config/config-example.js
@@ -3,7 +3,7 @@
 module.exports = {
   "port": "3000",
  // "webAdtPort": "3000",
-  "dbConnectionString": "mongodb://panacea:panacea01@localhost:27017/adt_msg_store",
+  "dbConnectionString": "mongodb://panacea:*******@localhost:27017/adt_msg_store",
   "minutes": "15",
   //read limitRecs records every minutesLog minutes (for log parser)
   "minutesLog" :"0.6",
